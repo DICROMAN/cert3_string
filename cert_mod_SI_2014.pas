f880829dbb9db5f067d7f461c51d0b935d35758b
DICROMAN <dayermanvride@gmail.com>
2016-07-01 00:58:49 -0300

    cert3
    strings

---
+++ cert_mod_SI_2014.pas
@@ -0,0 +1,101 @@
+program significados;
+uses crt;
+var
+entrada,salida,pregunta,pal,sin,lim,sin2,signi : string;
+bus,bus_dp,h,sinp,bus_ig,i,q,q2,es,es2,k,cont,w,bus_sl : byte;
+
+
+procedure limpiador;
+  begin
+    q:=pos(pregunta,entrada);
+    q:=q - 1;
+   { lim:=delete(entrada,1,q);}
+  end;
+
+procedure busqueda;
+  begin
+     h:=0;
+     bus:=pos(pregunta,entrada);
+     {bus_dp=busqueda dos puntos}
+     bus_dp:=pos(':',entrada);
+    { bus_dp:=bus_dp - 1;}
+     h:=bus_dp-bus;
+     pal:=copy(entrada,bus,h)
+
+  end;
+
+
+
+
+procedure sinonimos;
+  begin
+    bus_ig:=pos('=',entrada);
+    i:=bus_ig-bus_dp;
+
+    sin:=copy(entrada,bus_dp,i);
+    cont:=0;
+
+
+    for k:=1 to length(sin)do
+      begin
+        if(sin[k]=' ') then
+          cont:=cont+1;
+
+      end;
+    for k:=1 to cont+1 do
+    begin
+      if(k<>cont+1) then
+        begin
+          sin2:=copy(sin,1,pos(' ',sin));
+          write('    ',k,')',sin2);
+          delete(sin,1,pos(' ',sin));
+        end
+      else
+        begin
+          sin2:=sin;
+          writeln('    ',i,')',sin2);
+          delete(sin,1,pos(' ',sin));
+        end;
+    end;
+  end;
+
+procedure significado;
+  begin
+     w:=0;
+     bus_ig:=pos('=',entrada);
+     bus_ig:=bus_ig+1;
+
+
+     bus_sl:=pos('/',entrada);
+     w:=bus_sl-bus_ig;
+     signi:=copy(entrada,bus_ig,w)
+  end;
+
+
+
+begin
+  clrscr;
+  entrada:='';
+  salida:='';
+  writeln('ingrese datos segun el siguiente ejemplo; ');
+  writeln('  /Palabra1:Sinonimos=Significado/Palabra2:Sin..');
+ { readln(entrada); }
+  entrada:='/puerco:marrano=animal cochinito/casa:hogar vivienda lar=lugar en que se vive/' ;
+
+  writeln('Ingrese palabra que desea buscar');
+  readln(pregunta);
+
+  entrada[1]:='*';
+  busqueda;
+  significado;
+
+  salida:=salida + pal;
+
+  writeln('Palabra buscada: ',pal);
+  writeln('Significado:     ',signi);
+  writeln('Sinonimos: ');
+  sinonimos;
+  readln();
+
+
+end.
